fb1fe3936e142ca2811c909499e0544cc189b692
Jonas Carlsson <jonas@jonasonline.se>
2019-06-02 22:05:24 +0200

    misc

--- recon.py
+++ recon.py
@@ -1,7 +1,7 @@
 import json, os, subprocess, shutil
 
 firstRun = True
-with open('programs.json') as programsFile:
+with open('programs.temp.json') as programsFile:
     programs = json.load(programsFile)
     
     for program in programs['programs']:
@@ -18,11 +18,18 @@ with open('programs.json') as programsFile:
                 if 'url' in target:
                     print('Nothing to do here')
                 elif 'domain' in target:
-                    print(target['domain'])
                     domainBase = target['domain'].replace('*.','')
+                    
+                    #run amass
                     arguments = '-d ' + domainBase + ' -json ./output/' + programName + '/amass/' + domainBase + '.json -log ./output/' + programName + '/amass/' + domainBase + '.log.txt -do ./output/' + programName + '/amass/' + domainBase + '_data.json'
                     print(arguments)
-                    #subprocess.run('amass ' + arguments, shell=True)
+                    subprocess.run('amass ' + arguments, shell=True)
+
+                    #run subfinder
+                    subfinderArguments = '-d ' + domainBase + ' -o ./output/' + programName + '/subfinder/' + domainBase + '.json -oJ -t 10 -v -b -w ../wordlists/subdomains/jhaddix_all.txt -r 1.1.1.1, 8.8.8.8' 
+                    subprocess.run('./subfinder ' + arguments, shell=True)
+
+
         #Processing unique names
         for filename in os.listdir(amassFolder):
             if filename.endswith('.json') and not filename.endswith('_data.json'):
@@ -40,16 +47,17 @@ with open('programs.json') as programsFile:
             json.dump(sorted(uniqueDomains), f)
 
         #compare old and new current domains
-        with open('./output/' + programName + '/sortedDomains.json', 'r') as current:
-            currentData = json.load(current)
-            currentDataSet = set(currentData)
-            with open('./output/' + programName + '/sortedDomains.json.old', 'r') as old:
-                oldData = json.load(old)
-                oldDataSet = set(oldData)
-                for domain in currentDataSet:
-                    if domain not in oldDataSet and firstRun == False:
-                        print('New domain for ' + programName + ': ' + domain)
-        
+        if os.path.isfile('./output/' + programName + '/sortedDomains.json.old'):
+            with open('./output/' + programName + '/sortedDomains.json', 'r') as current:
+                currentData = json.load(current)
+                currentDataSet = set(currentData)
+                with open('./output/' + programName + '/sortedDomains.json.old', 'r') as old:
+                    oldData = json.load(old)
+                    oldDataSet = set(oldData)
+                    for domain in currentDataSet:
+                        if domain not in oldDataSet and firstRun == False:
+                            print('New domain for ' + programName + ': ' + domain)
+            
         #add domains to incremental domain list
         with open('./output/' + programName + '/sortedDomains.json', 'r') as current:
             currentData = json.load(current)
@@ -59,20 +67,5 @@ with open('programs.json') as programsFile:
                 incDomains = set(line.strip() for line in inc)
                 for domain in currentDataSet:
                     if domain not in incDomains:
+                        print('Adding domain ' + domain + ' to incremental list for ' + programName)
                         inc.write("%s\n" % domain)
-                
-                    
-
-
-
-        
-
-        #with open('amass_output/data.json') as amassOut:
-        #    for line in amassOut:
-        #    output = json.loads(line)
-                    
-
-
-
-        
-    
