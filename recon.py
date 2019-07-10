bebc8b2ae373557002c32274170e2cc711e32a6a
Jonas Carlsson <jonas@jonasonline.se>
2019-07-10 14:38:20 +0200

    refactoring for amass version 3

--- recon.py
+++ recon.py
@@ -9,44 +9,48 @@ with open('programs.json') as programsFile:
         programName = program['programName']
         amassFolder = './output/' + programName + '/amass'
         os.makedirs(amassFolder, exist_ok=True, )
-        #Saving old files for comparison 
-        for filename in os.listdir(amassFolder):
-            if not filename.endswith('.old'):
-                shutil.copy(amassFolder + '/' + filename, amassFolder + '/' + filename + '.old')
+                    
         for target in program['scope']:
             if target['inScope'] == True:
                 if 'url' in target:
-                    print('Nothing to do here')
+                    print(target['url'] + ': No URL Processing implemented.')
                 elif 'domain' in target:
                     domainBase = target['domain'].replace('*.','')
                     
+                    #Saving old files for comparison 
+                    domainFolder = amassFolder + "/" + domainBase
+                    for filename in os.listdir(domainFolder):
+                        if not filename.endswith('.old'):
+                            shutil.copy(domainFolder + '/' + filename, domainFolder + '/' + filename + '.old')
+
                     #run amass
-                    arguments = '-d ' + domainBase + ' -json ./output/' + programName + '/amass/' + domainBase + '.json -log ./output/' + programName + '/amass/' + domainBase + '.log.txt -do ./output/' + programName + '/amass/' + domainBase + '_data.json'
-                    print(arguments)
-                    subprocess.run('amass ' + arguments, shell=True)
+                    arguments = '-d ' + domainBase + ' -dir ./output/' + programName + '/amass/' + domainBase + '/'
+                    #print(arguments)
+                    #subprocess.run('amass enum ' + arguments, shell=True)
 
                     #run subfinder
                     subfinderArguments = '-d ' + domainBase + ' -o ./output/' + programName + '/subfinder/' + domainBase + '.json -oJ -t 10 -v -b -w ../wordlists/subdomains/jhaddix_all.txt -r 1.1.1.1, 8.8.8.8' 
-                    subprocess.run('./subfinder ' + arguments, shell=True)
+                    #print(subfinderArguments)
+                    #subprocess.run('./subfinder ' + arguments, shell=True)
 
+                    #Processing unique names
+                    for filename in os.listdir(domainFolder):
+                        if filename.endswith('.json') and not filename.endswith('_data.json'):
+                            with open(domainFolder + '/' + filename) as amassOut:
+                                for line in amassOut:
+                                    try:    
+                                        output = json.loads(line)
+                                        uniqueDomains.add(output['name'])
+                                    except:
+                                        print('Error')
+                    
 
-        #Processing unique names
-        for filename in os.listdir(amassFolder):
-            if filename.endswith('.json') and not filename.endswith('_data.json'):
-                with open(amassFolder + '/' + filename) as amassOut:
-                    for line in amassOut:
-                        try:    
-                            output = json.loads(line)
-                            uniqueDomains.add(output['name'])
-                        except:
-                            print('Error')
+        #compare old and new current domains
         if os.path.isfile('./output/' + programName + '/sortedDomains.json'):
             firstRun = False
             shutil.copy('./output/' + programName + '/sortedDomains.json', './output/' + programName + '/sortedDomains.json.old')
         with open('./output/' + programName + '/sortedDomains.json', 'w') as f:
             json.dump(sorted(uniqueDomains), f)
-
-        #compare old and new current domains
         if os.path.isfile('./output/' + programName + '/sortedDomains.json.old'):
             with open('./output/' + programName + '/sortedDomains.json', 'r') as current:
                 currentData = json.load(current)
