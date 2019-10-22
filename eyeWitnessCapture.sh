bff03a7767f9a79b9d5b139949e754efba76ba51
Jonas Näsman <jonas@jonasonline.se>
2019-10-23 01:12:50 +0200

    adding parameters to eyewitness

--- eyeWitnessCapture.sh
+++ eyeWitnessCapture.sh
@@ -1,4 +1,4 @@
 #find ./output/$1 -name \*.incremental.txt -exec cat {} > output/$1/eyewitness/allContentIncremental.txt \;
 cp ${PWD}/output/$1/incrementalContent.txt ${PWD}/output/$1/eyewitness/
 docker build --build-arg user=$USER --tag eyewitness lib/EyeWitness
-docker run --rm -it -v ${PWD}/output/$1/eyewitness:/tmp/EyeWitness eyewitness --web -f /tmp/EyeWitness/incrementalContent.txt
+docker run --rm -it -v ${PWD}/output/$1/eyewitness:/tmp/EyeWitness eyewitness --web -f /tmp/EyeWitness/incrementalContent.txt --timeout 20 --prepend-https
