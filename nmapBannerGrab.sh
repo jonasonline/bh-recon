da9f621ae9fc670392dc1f77bb510991ffdde863
Jonas Näsman <jonas@jonasonline.se>
2019-11-07 11:32:35 +0100

    bug fix

--- nmapBannerGrab.sh
+++ nmapBannerGrab.sh
@@ -1,1 +1,1 @@
-nmap -sS -sV -p $1 -v -n -Pn --script banner $2 -oG$PWD/output/$3/nmap/$1@$2.txt.grep -oN$PWD/output/$3/nmap/$1@$2.txt
+nmap -sS -sV -p $1 -v -n -Pn --script banner $2 -oG $PWD/output/$3/nmap/$1@$2.txt.grep -oN $PWD/output/$3/nmap/$1@$2.txt
