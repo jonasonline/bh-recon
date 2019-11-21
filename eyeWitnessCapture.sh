578f21ded686d645663fc7471e6fe7061cd6dd4f
Jonas Näsman <jonas@jonasonline.se>
2019-11-21 22:47:25 +0100

    debug

--- eyeWitnessCapture.sh
+++ eyeWitnessCapture.sh
@@ -1,3 +1,3 @@
 #input program name ($1), input file name ($2), output directory name ($3)
-cp ${PWD}/output/$1/$2 ${PWD}/output/$1/eyewitness/
+cp ${PWD}/output/$1/$2 ${PWD}/output/$1/eyewitness/$3
 docker run --rm -it -v ${PWD}/output/$1/eyewitness/$3:/tmp/EyeWitness eyewitness --web -f /tmp/EyeWitness/$2 --timeout 20 --prepend-https
