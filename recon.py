6b5b4e092ea5fdfce5d9ea9abe2083dc0a58ae0c
Jonas Näsman <jonas@jonasonline.se>
2020-05-02 13:42:23 +0200

    Preventing massdns when nodomainrecon flag is set

--- recon.py
+++ recon.py
@@ -339,7 +339,7 @@ def processProgram(program):
             subprocess.run('cat ' + incrementalDomainsFile + ' | httprobe > ' + liveHttpDomainsFile, shell=True)
             print("Done running httprobe")
 
-        if args.nomassdns == None:
+        if args.nomassdns == None OR args.nodomainrecon == True:
             with open(incrementalDomainsFile, 'r') as incrementalDomains:
                 incrementalDomains.seek(0)
                 domainNameList = set([])
