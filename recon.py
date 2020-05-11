504f62d4a366246ff30332902e75c2b7f849d494
Jonas Näsman <jonas@jonasonline.se>
2020-05-11 12:58:18 +0200

    Bug fix

--- recon.py
+++ recon.py
@@ -230,7 +230,7 @@ def processProgram(program):
                 else:
                     rootDomainsInScopeFile.write("%s" % rootDomain)
         #run amass
-        amassArguments = ' -df ./output/' + programName + '/rootDomainsInScope.txt -dir ./output/' + programName + '/amass/ --json ./output/LINE/amass/amass_' + programName + ' .json -r 9.9.9.9, 8.8.8.8, 1.1.1.1'
+        amassArguments = ' -df ./output/' + programName + '/rootDomainsInScope.txt -dir ./output/' + programName + '/amass/ --json ./output/LINE/amass/amass_' + programName + '.json -r 9.9.9.9, 8.8.8.8, 1.1.1.1'
         if args.nodomainrecon == None:
             print("Starting Amass for program: " + programName)
             #print(amassArguments)
